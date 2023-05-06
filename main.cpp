6f01fe768407be1b46ab50721b9b81c3f53635b8
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-05-06 22:01:43 +0300

    working algorithm :)

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ class Point3D
     double x, y, z;
 
 public:
+    Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
+
     void setX(double x)
     {
         this->x = x;
@@ -99,12 +101,60 @@ class FileManager
 public:
     static Point3D readPoint3D(File &file, int position)
     {
-        Point3D point;
+        Point3D point = Point3D(0, 0, 0);
         file.getFileStream().seekg(position);
         file.getFileStream() >> point;
         return point;
     }
 };
+
+void CalculateTriangle(vector<Point3D> &points)
+{
+    // Point3D point1, point2, point3, point12, point13;
+    // point12 and point13 are vectors of points
+    // point12 -> vector of point 1 and point 2
+    // point13 -> vector of point 1 and point 3
+    Point3D point1 = points[0];
+    Point3D point2 = points[1];
+    Point3D point3 = points[2];
+    // point12 = (x2-x1, y2-y1, z2-z1)
+    Point3D point12 = Point3D(point2.getX() - point1.getX(), point2.getY() - point1.getY(), point2.getZ() - point1.getZ());
+    // cout << point12 << endl;
+
+    //// point13 = (x3-x1, y3-y1, z3-z1)
+    Point3D point13 = Point3D(point3.getX() - point1.getX(), point3.getY() - point1.getY(), point3.getZ() - point1.getZ());
+    // cout << point13 << endl;
+
+    // point12 * point13 (scalar equation)
+
+    // X:
+    // |point12y point12z|
+    // |point13y point13z|
+    int newPointX = (point12.getY() * point13.getZ()) - (point12.getZ() * point13.getY());
+    cout << "X: " << newPointX << endl;
+
+    // Y:
+    // |point12z point12x|
+    // |point13z point13x|
+    int newPointY = (point12.getZ() * point13.getX()) - (point12.getX() * point13.getZ());
+    cout << "Y: " << newPointY << endl;
+
+    // Y:
+    // |point12x point12y|
+    // |point13x point13y|
+    int newPointZ = (point12.getX() * point13.getY()) - (point12.getY() * point13.getX());
+    cout << "Z: " << newPointZ << endl;
+
+    // check if it is equal to the null vector
+    if (newPointX != 0 || newPointY != 0 || newPointZ != 0)
+    {
+        cout << "These points can form a triangle :)" << endl;
+    }
+    else
+    {
+        cout << "These points cannot form a triangle :(" << endl;
+    }
+}
 
 int main()
 {
@@ -114,11 +164,11 @@ int main()
     while (!file.getFileStream().eof())
     {
         Point3D point = FileManager::readPoint3D(file, file.getFileStream().tellg());
-        cout << point << endl;
+        // cout << point << endl;
         points.push_back(point);
     }
 
-    // it is working :)
+    CalculateTriangle(points);
 
     return 0;
 }
