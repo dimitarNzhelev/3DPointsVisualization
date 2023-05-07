54271e3955320a01f5b7528382bb23e3f4ee4a16
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-05-07 10:25:53 +0300

    add images to ReadMe file and changed window size

--- main.cpp
+++ main.cpp
@@ -214,7 +214,7 @@ int main(int argc, char **argv)
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(640, 480);
+    glutInitWindowSize(1100, 1100);
     glutCreateWindow("3DPointsVisualization");
 
     glutDisplayFunc(display);
